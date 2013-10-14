8d247d39a14e69fc63b4fef59e5a8aa3778ea0b6
jesters320 <butler.brian.c@gmail.com>
2013-10-14 12:21:09 -0400

    Add header, footer, layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,13 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	subject { page }
 
-  describe "Home page" do
+	describe "Home page" do
+		before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-	
-	it "should have the right title" do
-	  visit '/static_pages/home'
-	  expect(page).to have_title("Sample App")
+		it { should have_content('Sample App') }
+		it { should have_title(full_title('')) }
+		it { should_not have_title('| Home') }
 	end
-	
-  end
 end

880a4d00e885e85324e54addeffef4be8f65cefb
jesters320 <butler.brian.c@gmail.com>
2013-10-15 22:51:03 -0400

    Making change to push

--- lib/yahoofinance.rb
+++ lib/yahoofinance.rb
@@ -4,6 +4,7 @@ require 'JSON'
 require 'uri'
 
 class YahooFinance
+  include HTTParty
   
 	@@base_uri = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D'MSFT'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
 

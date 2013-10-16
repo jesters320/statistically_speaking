87d817aaba5431daeede25301205d7220cb441a9
jesters320 <butler.brian.c@gmail.com>
2013-10-15 22:58:57 -0400

    Finally getting rid of these files

--- lib/yahoofinance.rb
+++ lib/yahoofinance.rb
@@ -8,7 +8,6 @@ class YahooFinance
   
 	@@base_uri = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D'MSFT'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
 
-	#not sure 
 	def self.getQuote
 		response = HTTParty.get(@@base_uri)
 		if response["query"]["results"] == nil
